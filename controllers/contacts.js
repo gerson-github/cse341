ebcfeee0c116cbfc4505e68a219457b2e2e80cff
gerson <gersonmoliver@gmail.com>
2025-05-16 15:50:32 -0700

    implementado put, delete, post

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -18,3 +18,52 @@ exports.getContacts = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 };
+
+//Post
+exports.createContact = async (req, res) => {
+    try {
+        const { id, firstName, lastName,  email, favoriteColor, birthday } = req.body;
+        const addContact = new Contacts({ id, firstName, lastName,  email, favoriteColor, birthday });
+        await addContact.save();
+        res.status(201).json(addContact)
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
+
+//Put
+exports.updateContact = async (req, res) => {
+    try {
+        const updContact =await Contacts.findOneAndUpdate(
+            {id: req.params.id},
+            req.body,
+            { new: true} // ret updated doc
+        );
+
+        if (!updContact) {
+            return res.status(404).json({ error: "record not found to update !!"});
+        }
+
+        res.json(updContact);
+
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
+
+//Delete
+exports.deleteContact = async (req, res) => {
+    try {
+        const delContact = await Contacts.findOneAndDelete ({ id: req.params.id })
+
+        if (!delContact) {
+            return res.status(404).json({ error: "contato nao existe !!"});
+        }
+
+        res.json( { message: " Deleted Successfuly !"})
+
+    } catch (err) {
+        res.status(500).json({error: err.message});
+    }
+};
+
--- models/contacts.js
+++ models/contacts.js
@@ -2,10 +2,20 @@ const mongoose = require("mongoose");
 
 // Defina o schema para a coleção "sales"
 const saleSchema = new mongoose.Schema({
-  name: String, // exemplo de campo
-  value: Number, // exemplo de campo
+  id: String,
+  firstName: String,
+  lastName: String,
+  email: String,
+  favoriteColor: String,
+  birthday: Date,
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  //name: String, // exemplo de campo
+  //value: Number, // exemplo de campo
   // adicione outros campos que sua coleção 'sales' tenha
-  date: { type: Date, default: Date.now }, // exemplo de um campo adicional
+  //date: { type: Date, default: Date.now }, // exemplo de um campo adicional
   // outros campos conforme necessário...
 });
 
