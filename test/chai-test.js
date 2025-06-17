0043c0f60076c89a10429ccf1a8bc02c3e2c97ac
gerson <gersonmoliver@gmail.com>
2025-06-17 13:31:35 -0700

    adicionei mais item para testes

--- test/chai-test.js
+++ test/chai-test.js
@@ -1,31 +1,41 @@
-require("dotenv").config();
 const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
 
-chai.use(chaiHttp);
 
-const API_URL = process.env.API_URL;
+chai.use(chaiHttp);
+console.log(chaiHttp);
+
 
 describe("GET /users", () => {
-  it("should return 200 OK", (done) => {
+  
+
+  it("should return all users", (done) => {
+  
+
     chai
-      .request(API_URL)
-      .get("/")
+      .request("http://localhost:3000")
+      .get("/users")
       .end((err, res) => {
         expect(res).to.have.status(200);
+        expect(res.body).to.be.an("array");
         done();
       });
   });
 });
 
+
 describe("GET /products", () => {
-  it("should return 200 OK", (done) => {
+  
+
+  it("should return all product", (done) => {
+    
     chai
-      .request(API_URL)
-      .get("/")
+      .request("http://localhost:3000")
+      .get("/users")
       .end((err, res) => {
         expect(res).to.have.status(200);
+        expect(res.body).to.be.an("array");
         done();
       });
   });
